45188105f7e1ce75b736ddf5e4e095a98de7271a
Goce Jovancevski <goki.github@gmail.com>
2024-10-01 20:28:25 +0200

    upload models

--- middlewares/storageMiddleware.js
+++ middlewares/storageMiddleware.js
@@ -1,10 +1,27 @@
 const multer = require("multer");
+const path = require('path');
 
 const memoryStorage = multer.memoryStorage();
 
 const diskStorage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, "uploads/"); // Destination folder for uploaded files
+		cb(null, "uploads/images/"); // Destination folder for uploaded files
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+		cb(
+			null,
+			file.fieldname +
+				"-" +
+				uniqueSuffix +
+				path.extname(file.originalname)
+		); // File name format
+	},
+});
+
+const diskStorageModel = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "uploads/models/"); // Destination folder for uploaded files
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
@@ -24,7 +41,8 @@ const uploadFile = (memory = false)=>{
     storage: storage,
     limits: { fileSize: 5 * 1024 * 1024 }, // Max file size: 5MB
     fileFilter: (req, file, cb) => {
-      const allowedTypes = /jpeg|jpg|png|gif/;
+      const allowedTypes = /jpeg|jpg|png|gif|glb|gltf/;
+      console.log("file.originalname",file.originalname, file.mimetype, file)
       const extname = allowedTypes.test(
         path.extname(file.originalname).toLowerCase()
       );
@@ -39,4 +57,26 @@ const uploadFile = (memory = false)=>{
   });
 }
 
-module.exports = uploadFile;
+const uploadModelFile = (memory = false)=>{
+  const storage = memory ? memoryStorage : diskStorageModel
+  return multer({
+    storage: storage,
+    limits: { fileSize: 100 * 1024 * 1024 }, // Max file size: 5MB
+    fileFilter: (req, file, cb) => {
+      const allowedTypes = /glb|gltf/;
+      console.log("file.originalname",file.originalname, file.mimetype, file)
+      const extname = allowedTypes.test(
+        path.extname(file.originalname).toLowerCase()
+      );
+      const mimetype = allowedTypes.test(file.mimetype);
+  
+      if (extname && mimetype) {
+        return cb(null, true);
+      } else {
+        cb(new Error("Only 3D models (glb, gltf) are allowed"));
+      }
+    },
+  });
+}
+
+module.exports = {uploadFile,uploadModelFile};
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const uploadFile  = require("../middlewares/storageMiddleware");
+const {uploadFile,uploadModelFile}  = require("../middlewares/storageMiddleware");
 const { singleFileUpload, multipleFilesUpload, uploadFileInMemory } = require("../controllers/fileController");
 
 const upload = uploadFile()
 const uploadMemory = uploadFile(true)
+const uploadModel = uploadModelFile()
+const uploadMemoryModel = uploadModelFile(true)
 
 router.post("/upload", upload.single("file"), singleFileUpload);
 router.post('/upload-multiple', upload.array('files', 5), multipleFilesUpload);
 router.post('/upload-memory', uploadMemory.single('file'), uploadFileInMemory);
+
+router.post("/model/upload", uploadModel.single("file"), singleFileUpload);
+router.post('/model/upload-multiple', uploadModel.array('files', 5), multipleFilesUpload);
+router.post('/model/upload-memory', uploadMemoryModel.single('file'), uploadFileInMemory);
 
 module.exports = router
